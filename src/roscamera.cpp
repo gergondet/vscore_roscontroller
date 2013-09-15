225890613b22dca416bb316e7bde1f7291803d60
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2013-09-15 19:09:56 +0900

    Handle depth camera

--- src/roscamera.cpp
+++ src/roscamera.cpp
@@ -4,7 +4,7 @@ namespace visionsystem
 {
 
 ROSCamera::ROSCamera()
-: img(0), _img_size(640, 480), _active(false), _fps(30), _name("ros-unconfigured"),
+: img(0), img_d(0), _img_size(640, 480), _active(false), _fps(30), _name("ros-unconfigured"),
   _topic("/camera/rgb/image_color"), _frame(0), _buffersize(100)
 {
 }
@@ -15,10 +15,21 @@ ROSCamera::~ROSCamera()
 
 void ROSCamera::imageCallback(const sensor_msgs::ImageConstPtr & msg)
 {
-    if(!img)
+    if(!img && msg->encoding == "bgr8")
     {
         _img_size = vision::ImageRef(msg->width, msg->height);
         img = new vision::Image<uint32_t, vision::RGB>(_img_size);
+        _prev_t = msg->header.stamp;
+        _buffer.clear();
+        for( unsigned int i = 0; i < _buffersize; ++i )
+        {
+            _buffer.enqueue( new Frame( get_coding(), get_size() ) );
+        }
+    }
+    else if(!img_d)
+    {
+        _img_size = vision::ImageRef(msg->width, msg->height);
+        img_d = new vision::Image<uint16_t, vision::DEPTH>(_img_size);
         _prev_t = msg->header.stamp;
         _buffer.clear();
         for( unsigned int i = 0; i < _buffersize; ++i )
@@ -32,9 +43,19 @@ void ROSCamera::imageCallback(const sensor_msgs::ImageConstPtr & msg)
         _fps = 1e9/dt.nsec;
         _prev_t = msg->header.stamp;
     }
-    for(size_t i = 0; i < img->pixels; ++i)
+    if(img)
     {
-        img->raw_data[i] = msg->data[3*i+2] + (msg->data[3*i+1] << 8) + (msg->data[3*i] << 16);
+        for(size_t i = 0; i < img->pixels; ++i)
+        {
+            img->raw_data[i] = msg->data[3*i+2] + (msg->data[3*i+1] << 8) + (msg->data[3*i] << 16);
+        }
+    }
+    else
+    {
+        for(size_t i = 0; i < img_d->pixels; ++i)
+        {
+            img_d->raw_data[i] = msg->data[2*i] + (msg->data[2*i+1] << 8);
+        }
     }
     _frame = msg->header.seq;
 }
--- src/roscamera.h
+++ src/roscamera.h
@@ -21,14 +21,26 @@ public:
 
     void imageCallback(const sensor_msgs::ImageConstPtr & msg);
 
-    unsigned char * get_data() { return (unsigned char *)(img->raw_data); }
+    unsigned char * get_data()
+    {
+        if(img)
+            return (unsigned char *)(img->raw_data);
+        else
+            return (unsigned char *)(img_d->raw_data);
+    }
 
     std::string get_topic() { return _topic; }
     
     /* Camera methods to implement */
     vision::ImageRef get_size() { return _img_size; }
     bool is_active() { return _active; }
-    visionsystem::FrameCoding get_coding() { return VS_RGB32; }
+    visionsystem::FrameCoding get_coding()
+    {
+        if(img)
+            return VS_RGB32;
+        else
+            return VS_DEPTH16;
+    }
     float get_fps() { return _fps; }
     std::string get_name() { return _name; }
     unsigned int get_frame() { return _frame; }
@@ -37,6 +49,7 @@ private:
     void parse_config_line( std::vector<std::string> & line );
 
     vision::Image<uint32_t, vision::RGB> * img;
+    vision::Image<uint16_t, vision::DEPTH> * img_d;
     vision::ImageRef _img_size;
     bool _active;
     float _fps;
